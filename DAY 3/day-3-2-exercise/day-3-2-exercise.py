2374cfebf0a0ebda40c222d2b1229d7cdccaddf0
wirededdy <hiamanosei@gmail.com>
2022-10-18 22:54:25 +0000

    Merge remote-tracking branch 'origin/main'

--- DAY 3/day-3-2-exercise/day-3-2-exercise.py
+++ DAY 3/day-3-2-exercise/day-3-2-exercise.py
@@ -4,7 +4,25 @@ weight = float(input("enter your weight in kg: "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+bmi_calc = weight/(height**2)
 
+bmi = round(bmi_calc,0)
 
+# print(bmi)
+
+if bmi < 18.5:
+    print(f"Yor bmi is {bmi} and Under you are underweight")
+
+elif bmi < 25:
+    print(f"Yor bmi is {bmi} and you are normal weight")
+
+elif bmi < 30:
+    print(f"Yor bmi is {bmi} and you are slightly overweight")
+
+elif bmi < 35:
+    print(f"Yor bmi is {bmi} and you are obese")
+
+else:
+    print(f"Yor bmi is {bmi} and you are clinically obese")
 
 
