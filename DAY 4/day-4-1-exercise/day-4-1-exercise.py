1f179b6730df1ffb799cc08f80b8eec9c0b3db5c
wirededdy <hiamanosei@gmail.com>
2022-10-22 16:01:53 +0000

    Merge remote-tracking branch 'origin/main'

---
+++ DAY 4/day-4-1-exercise/day-4-1-exercise.py
@@ -0,0 +1,16 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+flip = random.randint(0,1)
+if flip == 0:
+    print("Head")
+else:
+    print("Tail")
+
+
+
+
+
+
+
