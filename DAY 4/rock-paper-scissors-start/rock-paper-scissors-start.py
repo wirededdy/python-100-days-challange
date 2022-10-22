12594c8b3ce0be98a1f2dee0aaa2e28752090afd
wirededdy <hiamanosei@gmail.com>
2022-10-22 17:24:58 +0000

    "task day-4-3"

--- DAY 4/rock-paper-scissors-start/rock-paper-scissors-start.py
+++ DAY 4/rock-paper-scissors-start/rock-paper-scissors-start.py
@@ -27,13 +27,24 @@ scissors = '''
 
 #Write your code below this line 👇
 import random
-pick = random.randint(0,2)
-if pick == 0:
-    print(rock)
-    print("rock")
-elif pick == 1:
-    print(paper)
-    print("paper")
-else:
-    print(scissors)
-    print("scissors")
+player_pick =int(input("Plese make your pic 0for Rock, 1 for Paper and 2 for scissors"))
+com_pick = random.randint(0,2)
+# if com_pick == 0 and player_pick:
+#     print(rock)
+#     print("rock")
+# elif pick == 1:
+#     print(paper)
+#     print("paper")
+# else:
+#     print(scissors)
+#     print("scissors")
+if com_pick == player_pick:
+    print("Draw")
+elif com_pick == 0 and player_pick == 2:
+    print("Computer WON 😁")
+elif com_pick == 1 and player_pick == 0:
+    print("Computer WON 😁")
+elif com_pick == 2 and player_pick == 1:
+    print("Player 1 has WON 😁")
+elif com_pick < player_pick :
+    print("Player 1 has WON 😁")
