8df7615e87be56a9eb001e44d977522193eef8df
wirededdy <hiamanosei@gmail.com>
2022-10-22 12:16:07 +0000

    Merge remote-tracking branch 'origin/main'

--- DAY 3/day-3-4-exercise-3-end/day-3-4-exercise-3.py
+++ DAY 3/day-3-4-exercise-3-end/day-3-4-exercise-3.py
@@ -1,13 +1,33 @@
 # 🚨 Don't change the code below 👇
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L ")
-add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
+size = input("What size pizza do you want? S, M, or L ").lower()
+add_pepperoni = input("Do you want pepperoni? Y or N ").lower()
+extra_cheese = input("Do you want extra cheese? Y or N ").lower()
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+price = 0
+if size == "s":
+    price += 15
+    print("price is $15")
+    if add_pepperoni == "y":
+        price += 2
+        print("price is $2")
+elif size == "m":
+    price += 20
+    print("price is $20")
+    if add_pepperoni == "y":
+        price += 3
+        print("price is $3")
+else:
+    price += 25
+    print("price is $25")
+    if add_pepperoni == "y":
+        price += 3
+        print("price is $3")
 
-
-
-
+if extra_cheese == "y":
+    price += 1
+Total = price
+print(f"Your total cost is {price}")
 
