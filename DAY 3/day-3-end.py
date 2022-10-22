bb3e391fe3eaeef746b3bf6367fb0da9e3c0b4fb
wirededdy <hiamanosei@gmail.com>
2022-10-22 11:49:01 +0000

    Merge remote-tracking branch 'origin/main'

--- DAY 3/day-3-end.py
+++ DAY 3/day-3-end.py
@@ -4,11 +4,21 @@ height = int(input("What is your height in cm? "))
 if height >= 120:
     print("You can ride")
     age = int(input("What is your age"))
+    price =0
     if age > 18:
+        price =12
         print("You will pay $12")
     elif age < 12:
+        price =5
         print("You will pay $5")
     else:
+        price =7
         print("You will pay $7")
+    photos = int(input("Please do you want Photos? "))
+    if photos.lower() =="yes":
+        price+=3
+        print("You will pay $3")
+    total= price
+    print(f"Your total price is {price}")
 else:
     print("You can't ride")
