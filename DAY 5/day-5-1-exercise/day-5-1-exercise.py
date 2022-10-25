43ab58bac7ee0e90b3d7dc7f006712a1eb34662e
wirededdy <hiamanosei@gmail.com>
2022-10-25 00:03:38 +0000

    "task day-4-4"

--- DAY 5/day-5-1-exercise/day-5-1-exercise.py
+++ DAY 5/day-5-1-exercise/day-5-1-exercise.py
@@ -1,4 +1,4 @@
-# 🚨 Don't change the code below 👇
+# # 🚨 Don't change the code below 👇
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
@@ -6,7 +6,13 @@ for n in range(0, len(student_heights)):
 
 
 #Write your code below this row 👇
+length = len(student_heights)
+total = 0
 
+for n in range(length):
 
+  total += student_heights[n]
+avg = round(total/length)
 
-
+print(total)
+print(avg)
